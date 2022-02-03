e2c5f8b680720f36c0dd2967b25db181883e0495
keloholic <72310838+keloholic@users.noreply.github.com>
2022-02-02 23:42:07 -0600

    Added functionality to game and phrase files.

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,28 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+       this.missed = 0;
+       this.phrases = [new Phrase('Programming is a joy'),
+                       new Phrase('To write code is to solve problems'),
+                       new Phrase('You will fail everything you do not try'),
+                       new Phrase('Be tolerant with others and strict with yourself'),
+                       new Phrase('Make the mind tougher by exposing it to adversity'),
+                       new Phrase('Learn to be indifferent to what makes no difference'),
+                       new Phrase('Man conquers the world by conquering himself'),];
+       this.activePhrase = 'null';
+    }
+
+        /**
+    * Selects random phrase from phrases property
+    * @return {Object} Phrase object chosen to be used
+    */
+    getRandomPhrase() {
+        const randomNumber = Math.floor(Math.random() * this.phrases.length);
+        let phrase = this.phrases[randomNumber];
+        
+        return phrase;
+    };
+    };

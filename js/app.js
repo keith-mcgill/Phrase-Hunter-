d31319c188e482f8ecde055d15b800add8c07d76
keloholic <72310838+keloholic@users.noreply.github.com>
2022-02-03 18:54:21 -0600

    Added startButton functionality to the app.js file

--- js/app.js
+++ js/app.js
@@ -21,3 +21,11 @@ game.getRandomPhrase().addPhraseToDisplay();
 game.startGame();
 console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
 */
+
+let game;
+const startButton = document.querySelector('#btn__reset');
+
+startButton.addEventListener('click',() => {
+    game = new Game();
+    game.startGame();
+});

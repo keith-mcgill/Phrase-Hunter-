443f2f27aaaa3acde643dc20a7e520aecedcecab
keloholic <72310838+keloholic@users.noreply.github.com>
2022-02-07 18:08:10 -0600

    Removed test code

--- js/app.js
+++ js/app.js
@@ -1,26 +1,5 @@
 
 
-/*const logPhrase = (phrase) => {
-    console.log(`Phrase - phrase: `, phrase.phrase);
-    };
-    const game = new Game();
-    logPhrase(game.getRandomPhrase());
-    logPhrase(game.getRandomPhrase());
-    logPhrase(game.getRandomPhrase());
-    logPhrase(game.getRandomPhrase());
-    logPhrase(game.getRandomPhrase());
-
-    const hello = new Phrase('HEllo')
-    console.log(hello)
-  */  
-    /*const game = new Game();
-game.getRandomPhrase().addPhraseToDisplay();
-*/
-
-/*const game = new Game();
-game.startGame();
-console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
-*/
 
 let game;
 const startButton = document.querySelector('#btn__reset');
